9016ed570b353d8b20a5cfdb0b7bef702b3d10ec
gencsis <genesisesc26@gmail.com>
2025-10-29 15:15:22 -0500

    Temp Main Menu

---
+++ spanish-mon/main_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Phases/game_level.tscn")
+
+
+func _on_options_pressed() -> void:
+	#idk what to put here
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()
+
+	
+	
+	
